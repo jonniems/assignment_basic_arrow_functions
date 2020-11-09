df28f4aebf0caa1210f6cc7facc261a8d495b679
Jonnie <jonnie.muis@gmail.com>
2020-11-09 12:00:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// OPDRACHT A
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions();
+
+//OPDRACHT B
+const fivePlusSeven = () => 5 + 7;
+
+fivePlusSeven();
+
+//OPDRACHT C
+const myFunction = (a, b) => a + b;
+
+myFunction(5, 7);
+
+//OPDRACHT D
+const addFive = a => a + 5;
+
+addFive(7);
+
+//OPDRACHT E
+const createObject = () => ({ greeting: "hoi" });
+
+createObject();
